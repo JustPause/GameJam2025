30c76880f6a3e90b238f6f79fb4d96bfcfe3ea1c
shiv808 <shivshah2004@gmail.com>
2025-01-25 21:10:29 +0200

    removing duplicate, scenes which are un used/ broken

--- Code/basic_bubble.gd
+++
@@ -1,9 +0,0 @@
-extends CharacterBody2D
-
-@export var speed = 300
-
-func _process(delta: float) -> void:
-	get_parent().set_process(get_parent().get_process_() + speed * delta)
-	
-func kill() -> void:
-	call_deferred("queue_free")
