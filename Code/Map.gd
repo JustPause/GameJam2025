f98d87c335841683c792e9a0d61ab883d373ade4
JustPause <Justinaslolt@gmail.com>
2025-01-25 17:56:04 +0200

    Merge branch 'main' into Is-it-fix

--- Code/Map.gd
+++ Code/Map.gd
@@ -6,26 +6,23 @@ var tile_maps = []
 var target_position = Vector2(0, 0)  # Example position (x, y)
 
 func _ready():
-	tile_maps.append($Grass)
+	#tile_maps.append($Grass)
 	tile_maps.append($Path)
 	tile_maps.append($Casele)
 	tile_maps.append($Buldings)
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		var mouse_pos = get_global_mouse_position()
-		var tile = get_clicked_tile_power()
-		print(tile)
-
-func get_clicked_tile_power():
+func get_clicked_tile():
 
 	var returning = []
+	var clicked_cell
+	var data
 	for tile in tile_maps:
-		var clicked_cell = tile.local_to_map(tile.get_local_mouse_position())
-		var data = tile.get_cell_tile_data(clicked_cell)
+		clicked_cell = tile.local_to_map(tile.get_local_mouse_position())
+		data = tile.get_cell_tile_data(clicked_cell)
 		
-		print(data)
-
+		if data != null:
+			return "null"
+	return clicked_cell
 
 func convert_world_to_grid(mouse_pos):
 	return Vector2(0,0)
