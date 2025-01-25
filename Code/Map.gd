5cd9e6aa9c984db651cca4278e44e16e89de85c6
JustPause <Justinaslolt@gmail.com>
2025-01-25 16:34:22 +0200

    a

--- Code/Map.gd
+++ Code/Map.gd
@@ -4,27 +4,22 @@ var tile_maps = []
 
 # Coordinates where the player wants to place the object (example)
 var target_position = Vector2(0, 0)  # Example position (x, y)
-var root_node = get_parent()
-var control_node = root_node.get_
+var root_node
+var control_node
+var block_input = false
 
 func _ready():
 	#tile_maps.append($Grass)
 	tile_maps.append($Path)
 	tile_maps.append($Casele)
 	tile_maps.append($Buldings)
-
-func _input(event: InputEvent) -> void:
-	if should_block_input():
-		print("block")
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		var mouse_pos = get_global_mouse_position()
-		var tile = get_clicked_tile_power()
+	
+	control_node=get_parent().get_node("Camera2D/Ui")
 
 func should_block_input() -> bool:
 	return control_node.visible
 
 func get_clicked_tile_power():
-
 	var returning = []
 	for tile in tile_maps:
 		var clicked_cell = tile.local_to_map(tile.get_local_mouse_position())
@@ -36,6 +31,11 @@ func get_clicked_tile_power():
 			print("not free")
 	print("")
 
-
 func convert_world_to_grid(mouse_pos):
 	return Vector2(0,0)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		var mouse_pos = get_global_mouse_position()
+		var tile = get_clicked_tile_power()
+		print(control_node)
