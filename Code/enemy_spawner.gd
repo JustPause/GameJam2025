bcc9a36d7ee092f5ac3c6e2cfa1d0e13b40f448e
JustPause <Justinaslolt@gmail.com>
2025-01-26 03:49:31 +0200

    Boo

--- Code/enemy_spawner.gd
+++ Code/enemy_spawner.gd
@@ -1,17 +1,18 @@
 extends Path2D
 
 class Wave_data:
-	var amount_in_a_round: int
 	var pause_betwene: float
 	var units_count: int
-	var units: Array
+	var units: String
+	var round: int
 		
-	func _init(amount_in_a_round: int, units_count: int, units: Array):
-		self.amount_in_a_round = amount_in_a_round
+	func _init(units_count: int, pause_betwene: int, units: String, round:int):
+		self.pause_betwene=pause_betwene
 		self.units_count = units_count
 		self.units = units
+		self.round=round
 
-		self.pause_betwene = units_count / amount_in_a_round
+		self.pause_betwene = units_count / pause_betwene
 
 class Enemys:
 	var name_enemys: String
@@ -23,7 +24,7 @@ class Enemys:
 	var speed: float
 	var scene: PackedScene
 	
-	func _init(name_enemys, enemy_type, max_health, health, base_damage, max_size_growth, speed,scene):
+	func _init(name_enemys, enemy_type, max_health, health, base_damage, max_size_growth, speed, scene):
 		self.name_enemys = name_enemys
 		self.enemy_type = enemy_type
 		self.max_health = max_health
@@ -41,23 +42,39 @@ var spawn_enemies: bool = true
 var can_spawn: bool = true
 var count = 0
 var countdown_is_active = false;
-var timer_countDown:SceneTreeTimer;
+var timer_countDown: SceneTreeTimer;
 
-var wave=[]
+var wave = []
 var current_wave_index: int = 0
 var wave_timer: Timer
 var enemys = [
-	Enemys.new("Basic Bubble", "Air", 5.0, 5.0, 1, Vector2(0.3, 0.3), 600.0,basic_bubble_scene),
-	Enemys.new("Basic Bubble1", "Air", 5.0, 5.0, 1, Vector2(0.3, 0.3), 300.0,basic_bubble_scene),
-	Enemys.new("Basic Bubble2", "Air", 5.0, 5.0, 1, Vector2(0.3, 0.3), 300.0,basic_bubble_scene),
-	Enemys.new("Basic Bubble3", "Air", 5.0, 5.0, 1, Vector2(0.3, 0.3), 300.0,basic_bubble_scene),
-	Enemys.new("Basic Bubble4", "Air", 5.0, 5.0, 1, Vector2(0.3, 0.3), 300.0,basic_bubble_scene),
-	Enemys.new("Basic Bubble5", "Air", 5.0, 5.0, 1, Vector2(0.3, 0.3), 300.0,basic_bubble_scene)
+	Enemys.new("Basic Bubble", "Air", 5.0, 5.0, 1, Vector2(0.3, 0.3), 600.0, basic_bubble_scene),
+	Enemys.new("Basic Bubble1", "Air", 5.0, 5.0, 1, Vector2(0.3, 0.3), 300.0, basic_bubble_scene),
+	Enemys.new("Basic Bubble2", "Air", 5.0, 5.0, 1, Vector2(0.3, 0.3), 300.0, basic_bubble_scene),
+	Enemys.new("Basic Bubble3", "Air", 5.0, 5.0, 1, Vector2(0.3, 0.3), 300.0, basic_bubble_scene),
+	Enemys.new("Basic Bubble4", "Air", 5.0, 5.0, 1, Vector2(0.3, 0.3), 300.0, basic_bubble_scene),
+	Enemys.new("Basic Bubble5", "Air", 5.0, 5.0, 1, Vector2(0.3, 0.3), 300.0, basic_bubble_scene)
 ]
 
 func _ready() -> void:
-	wave.append(Wave_data.new(10, 10, ["Basic_Bubble"]))
-	wave.append(Wave_data.new(2, 10, ["Basic_Bubble"]))
+	wave.append(Wave_data.new(10, 10, "Basic_Bubble",0))
+
+	wave.append(Wave_data.new(3, 2, "Basic_Bubble",1))
+	wave.append(Wave_data.new(4, 6, "Advanced_Bubble",1))
+
+	wave.append(Wave_data.new(3, 4, "Advanced_Bubble",2))
+	wave.append(Wave_data.new(2, 3, "Ice_Bubble",2))
+	wave.append(Wave_data.new(3, 4, "Advanced_Bubble",2))
+
+	wave.append(Wave_data.new(2, 2, "Basic_Bubble",3))
+	wave.append(Wave_data.new(2, 3, "Advanced_Bubble",3))
+	wave.append(Wave_data.new(3, 2, "Basic_Bubble",3))
+	wave.append(Wave_data.new(2, 3, "Ice_Bubble",3))
+	wave.append(Wave_data.new(1, 3, "Advanced_Bubble",3))
+
+	wave.append(Wave_data.new(10, 10, "Boss",4))
+	
+	
 
 	spawn_time_interval = wave[0].pause_betwene
 
@@ -69,8 +86,13 @@ func spawn_enemy() -> void:
 	can_spawn = true
 
 func _physics_process(_delta: float) -> void:
-	if spawn_enemies and can_spawn and (wave[current_wave_index].amount_in_a_round > count):
+	if spawn_enemies and can_spawn and (wave[current_wave_index].units_count > count):
 
+		print("wave[current_wave_index].units_count ",wave[current_wave_index].units_count," count ",count)
+		if wave[current_wave_index + 1].round == wave[current_wave_index].round and wave[current_wave_index].units_count ==count+1:
+			print("Round " , current_wave_index)
+			current_wave_index=current_wave_index+1
+			count=0
 		spawn_enemy()
 		count += 1
 		can_spawn = false
@@ -78,13 +100,13 @@ func _physics_process(_delta: float) -> void:
 		get_tree().create_timer(spawn_time_interval).timeout.connect(_on_spawn_timer_timeout)
 	#if self.get_child_count() < wave[current_wave_index].amount_in_a_round:
 	
-	if self.get_child_count()==0 and can_spawn and !countdown_is_active:
+	if self.get_child_count() == 0 and can_spawn and !countdown_is_active:
 		print("Good")
 		on_wave_timer_timeout()
-		countdown_is_active=true
+		countdown_is_active = true
 	
-	if(timer_countDown !=null ):
-		if(timer_countDown.time_left!=0):
+	if (timer_countDown != null):
+		if (timer_countDown.time_left != 0):
 			print(timer_countDown.time_left)
 
 func _on_spawn_timer_timeout() -> void:
@@ -93,9 +115,9 @@ func _on_spawn_timer_timeout() -> void:
 func start_next_wave() -> void:
 	if current_wave_index < wave.size():
 		print("Starting wave ", current_wave_index + 1)
-		count = 0  # Reset enemy spawn count
-		spawn_enemies = true  # Allow enemy spawning
-		countdown_is_active =false
+		count = 0 # Reset enemy spawn count
+		spawn_enemies = true # Allow enemy spawning
+		countdown_is_active = false
 	else:
 		print("All waves completed!")
 		spawn_enemies = false
