1e75633f5c8f8ebd0763dd9aa2bab2931c901762
shiv808 <shivshah2004@gmail.com>
2025-01-26 13:05:54 +0200

    turret selection

--- Scene/Green Lands.gd
+++ Scene/Green Lands.gd
@@ -4,6 +4,7 @@ var map_node : Node2D
 const GRID_SIZE = Vector2(16, 16)
 var turret=[]
 var turret_index = -1
+var turret_index = -1
 var points = 200
 
 func _ready() -> void:
@@ -32,24 +33,17 @@ func add_turret(turret_name, grid_position: Vector2):
 	add_child(turret_name)
 
 func _input(event: InputEvent) -> void:
+	print(turret_index)
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		var tile = map_node.get_clicked_tile()
-	
-		var turret_t=turret[0].instantiate()
-		if typeof(tile) == TYPE_VECTOR2I and points>=turret_t.price and tile is Vector2i:
+		if turret_index > -1:
+			var tile = map_node.get_clicked_tile()
+		
+			var turret_t=turret[turret_index].instantiate()
 
-			place_tile_at_location(tile)
-			points=points-turret_t.pay()
-			add_turret(turret_t, tile)
-	elif event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
-		var tile = map_node.get_clicked_tile()
-	
-		var turret_t=turret[1].instantiate()
-		if typeof(tile) == TYPE_VECTOR2I and points>=turret_t.price and tile is Vector2i:
-
-			place_tile_at_location(tile)
-			points=points-turret_t.pay()
-			add_turret(turret_t, tile)
+			if typeof(tile) == TYPE_VECTOR2I and points>=turret_t.price and tile is Vector2i:
+				place_tile_at_location(tile)
+				points=points-turret_t.pay()
+				add_turret(turret_t, tile)
 			
 func place_tile_at_location(tile: Vector2i):
 	var buildings: TileMapLayer = map_node.find_child("Buldings")
